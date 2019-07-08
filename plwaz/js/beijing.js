6f4dfe245881507aba959bf176f66b1644b33072
寂寞的冷落 <exileme@163.com>
2019-07-08 16:32:06 +0800

    提交个人收藏的网址导航素材

---
+++ plwaz/js/beijing.js
@@ -0,0 +1,10 @@
+
+<!-- Begin 
+　　var timedate= new Date("August 9,2008"); 
+　　var times="2008北京奥运会"; 
+　　var now = new Date(); 
+　　var date = timedate.getTime() - now.getTime(); 
+　　var time = Math.floor(date / (1000 * 60 * 60 * 24)); 
+　　if (time >= 0) ;
+document.write("距"+times+"还有: <font color=red><b>"+time +"</b></font>天");
+// End -->
