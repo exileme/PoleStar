924fbb78b0a1996e68e5288f590807b3e7bbe429
寂寞的冷落 <exileme@163.com>
2019-10-28 16:40:49 +0800

    更新0.2版本，增加url判定，url不同，可能网页地址ID不同（上次提交是0.1）

--- fictionFontColor.user.js
+++ fictionFontColor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Add font color
 // @namespace    PoleStar
-// @version      0.1
+// @version      0.2
 // @description  为自己所看小说页面字体添加字体颜色
 // @author       PoleStar
 // @include      *://www.zhaishuyuan.com/*
@@ -11,10 +11,13 @@
 
 (function() {
     'use strict';
-    var divId=$('#content');
-    if(divId.length==1){
-       // alert("hello world");
-        divId.attr("style","font-size:26px;font-family:'黑体';color:#00F");
+    var url = window.location.host;
+    var divId=$('#content');//zhaishuyuan
+    if(url.indexOf('zhaishuyuan')!=-1){
+        if(divId.length==1){
+            // alert("hello world");
+            divId.attr("style","font-size:26px;font-family:'黑体';color:#00F");
+        }
     }
     // Your code here...
 })();
